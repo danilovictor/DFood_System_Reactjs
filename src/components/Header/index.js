6bce8092bfd4f320568831a898e8dd15de723ee9
Danilo Victor <danilo-almeida12@hotmail.com>
2022-02-16 11:38:32 -0300

    styledheader

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Container } from './styled'
+import { Container , Logo , SearchInput } from './styled'
+
+import logo from '../../assets/logo.png'
 
 const Header = ()=>{
     return (
         <div>
             <Container>
-                
+              <Logo src={logo}/>
+              <SearchInput type="text" placeholder="Digite o seu produto"/>
             </Container>
         </div>
     )
--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -1,5 +1,32 @@
 import styled from 'styled-components';
+import search from '../../assets/search.png'
 
 export const Container = styled.div`
+background-color:#136713;
+border-radius: 4px;
+padding: 20px;
+display:flex;
+justify-content:space-between;
+align-items: center;
+`;
+
+export const Logo = styled.img`
+ width: auto;
+ height:65px;
+`;
+
+export const SearchInput = styled.input`
+border:0;
+border-radius: 25px;
+width:0px;
+height:50px;
+background-color:white;
+background-image:url(${search});
+background-repeat:no-repeat;
+background-size:30px;
+background-position: 13px center;
+outline:0;
+padding-left: 50px;
+
 
 `;
