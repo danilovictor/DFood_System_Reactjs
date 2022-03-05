266303d6305fe2c5b6b0d6948ebea2dbf9c1bc32
Danilo Victor <danilo-almeida12@hotmail.com>
2022-03-04 23:20:02 -0300

    Cart page body

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,5 +1,5 @@
 import React , {useState} from 'react';
-import { CartArea , CartHeader , CartBody , CartIcon , CartText , CartBody} from './styled'
+import { CartArea , CartHeader , CartBody , CartIcon , CartText} from './styled'
 import cart from '../../assets/cart.png'
 import {useSelector} from 'react-redux';
 
@@ -8,17 +8,21 @@ const Cart = ()=>{
     const [opened , setOpened] = useState( false );
 
     const products = useSelector( state => state.cart.products);
+
+    const handleClickCart = () => {
+        setOpened( !opened);
+    }
     
     return(
         <div>
             <CartArea>
-                <CartHeader>
+                <CartHeader onClick={handleClickCart}>
                     <CartIcon src={cart}/>
                     <CartText> Meu Carrinho ({ products.length} )</CartText>
                 </CartHeader>
                 <CartHeader>
                     <CartBody opened={opened}>
-
+                        
                     </CartBody>
                 </CartHeader>
             </CartArea>
